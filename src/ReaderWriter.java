5444d3870819e45a78a1346b6d27d2631fd8f616
micha <micha@TABLET-IM20DSL1.lan>
2022-02-12 17:12:58 -0600

    Added Bottom 3

--- src/ReaderWriter.java
+++ src/ReaderWriter.java
@@ -7,12 +7,14 @@ public class ReaderWriter
 	File myFile;
 	
 	String[] top3;
+	String[] bottom3;
 	
 	public ReaderWriter(String name) throws IOException 
 	{
 		fileName = name;		
 		myFile = new File(fileName);
 		top3 = new String[3];
+		bottom3 = new String[3];
 	}
 	
 	public void appendToFile(String values) throws IOException 
@@ -37,7 +39,7 @@ public class ReaderWriter
 		Scanner myScanner;
 		try 
 		{
-			File file = new File("C:\\Users\\alexa\\git\\BlackJack\\test.txt");
+			File file = new File("C:\\Users\\micha\\git\\BlackJack3\\test.txt");
 			myScanner = new Scanner(file);
 			double firstValue = 0, secondValue = 0, thirdValue = 0;
 			
@@ -99,4 +101,74 @@ public class ReaderWriter
 		System.out.println(top3[1]);
 		System.out.println(top3[2]);
 	}
+	
+	public void bottomThree()
+	{
+		Scanner myScanner;
+		try 
+		{
+			File file = new File("C:\\Users\\alexa\\git\\BlackJack\\test.txt");
+			myScanner = new Scanner(file);
+			double firstValue = 0, secondValue = 0, thirdValue = 0;
+			
+			// myScanner.nextLine();
+			
+			while(myScanner.hasNextLine())
+			{
+				String line = myScanner.nextLine();
+				//System.out.println(line);
+				
+				if(!line.equals(""))
+				{
+					String[] splitLine = line.split(" ");
+					
+					//System.out.println(splitLine[0]);
+					
+					double net = Double.parseDouble(splitLine[0]);
+					
+					if(net < firstValue)
+					{
+						thirdValue = secondValue;
+						secondValue = firstValue;
+						firstValue = net;
+						
+						top3[2] = top3[1];
+						top3[1] = top3[0];
+						top3[0] = line;
+					}
+					else if (net < secondValue)
+					{
+						thirdValue = secondValue;
+						secondValue = net;
+						
+						top3[2] = top3[1];
+						top3[1] = line;
+					}
+					else if (net < thirdValue)
+					{
+						thirdValue = net;
+						
+						top3[2] = line;
+					}
+				}
+				
+			}
+			
+		} catch (FileNotFoundException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void printBottomThree()
+	{
+		System.out.println("Bottom 3 lines:");
+		System.out.println(bottom3[0]);
+		System.out.println(bottom3[1]);
+		System.out.println(bottom3[2]);
+	}
+	
+	
 }
